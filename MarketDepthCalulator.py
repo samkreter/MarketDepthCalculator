46fa5bd33f2b0a993de993b3d1e418a5ead3086d
Sam Kreter <sakfy6@mail.missouri.edu>
2015-05-01 06:10:26 -0500

    fixed the accuracy problem in the if statment

--- MarketDepthCalulator.py
+++ MarketDepthCalulator.py
@@ -12,15 +12,21 @@ class MarketDepthCalulator:
 		self.quan = 1
 
 
+	def pwd(self,sells):
+		for x in range(199,189):
+			print sells[x]
+
 	def chinaExchangeBuyCoins(self,money):
 		
 		okCoinStart_time = time.time()
 		okcoin_market_depth = json.loads(urllib2.urlopen(self.okcoin_market_depth_url).read())
 		print "okCoin Execution Time = ",time.time() - okCoinStart_time
+		self.pwd(okcoin_market_depth['asks']) ######################
 
 		btcChinaStart_time = time.time()
 		btcchina_market_depth = json.loads(urllib2.urlopen(self.btcchina_market_depth_url).read())
 		print "btcChina Execution Time = ",time.time() - btcChinaStart_time
+		self.pwd(btcchina_market_depth['asks'])########################
 
 		totalMarcketBuyData = dict()
 		totalMarcketBuyData['OKCoin'] = self.calculate_buy_bitcoins(money,okcoin_market_depth['asks'],okcoin_market_depth['bids'])
@@ -30,26 +36,38 @@ class MarketDepthCalulator:
 	def calculate_buy_bitcoins(self,money,sells,buys):
 		currMoney = money
 		coinsExchanged = 0
-		depth = 0
+		depth = 199
 
 		try:
 			while  currMoney > (sells[depth][self.price] * sells[depth][self.quan]) and currMoney > 0:
 				currMoney = currMoney - (sells[depth][self.price] * sells[depth][self.quan])
 				coinsExchanged = coinsExchanged + sells[depth][self.quan]
-				depth = depth + 1
+				depth = depth - 1
+				print "while loop coins: ",coinsExchanged
+				print "while loop money: ",currMoney
+				print "while loop price: ",sells[depth][self.price]
+				print "depth is ",depth
+				print ""
 		except:
 			print "unable to calculate due to lack of sellers"
 			print "depth reached was {0}".format(depth)
 			sys.exit()
 
-		if currMoney < sells[depth][self.price] and currMoney > 0:
+		print "Outside the ifstatmnet"
+		if currMoney > 0:
 			coinsExchanged = coinsExchanged + (currMoney / sells[depth][self.price])
+			print "if stat coins: ",coinsExchanged
+			print "if stat money: ",currMoney
+			print "if stat price: ", sells[depth][self.price]
+			print ""
 
 		#testing accuracy 
 		print "total bitcoins bought: {0}".format(coinsExchanged)
 		print "total market depth reached: {0}".format(depth)
 		print "total money spent: ${0}".format(money) 
+		print "average price: ${0}".format(money/coinsExchanged)
 		print " "
+
 
 		return dict(coinsExchanged=coinsExchanged,depth=depth)
 
